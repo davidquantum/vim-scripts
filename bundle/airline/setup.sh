d547fc1767b346fdf94afbd9789f04beedb02a24
David Pugal <david.pugal@gmail.com>
2015-03-06 16:02:12 -0800

    airline, ctrlp to buffers

--- bundle/airline/setup.sh
+++ bundle/airline/setup.sh
@@ -3,13 +3,14 @@
 DIR=$( pushd "$( dirname "$0" )" &>/dev/null && pwd && popd &>/dev/null)
 source $DIR/../../env.sh
 
-echo "Installing CtrlP"
+echo "Installing vim-airline"
 pushd $BUNDLE &>/dev/null
-checkinstall "ctrlp.vim"
+checkinstall "vim-airline"
 if [ -z $YESNO ] || [ $YESNO == "y" ]; then
-        storetobackup ctrlp.vim
-        git clone https://github.com/kien/ctrlp.vim.git
+        storetobackup vim-airline 
+        git clone https://github.com/bling/vim-airline
         echo "$DONE"
+
         appendfiles "vimrc-*" $DIR
 fi
 popd &>/dev/null
